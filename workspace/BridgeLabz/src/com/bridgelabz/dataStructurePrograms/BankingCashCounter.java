6f131f586ac9a670e6e2b88f58e6215241a31950
vishalrjpt02 <vishalrjpt19@gmail.com>
2019-11-22 12:33:00 +0530

    [datastructre].add BankingCashCounter.logicUpdated

---
+++ workspace/BridgeLabz/src/com/bridgelabz/dataStructurePrograms/BankingCashCounter.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.dataStructurePrograms;
+import com.bridgelabz.Datastructre.base.Queue;
+import com.bridgelabz.Utility.Util;
+public class BankingCashCounter {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("enter number of people that can be in queue");
+		int num=Util.readint();
+		int money=0;
+		String arr[]=new String[num];
+		Queue q1=new Queue();
+		for(int i=0;i<num;i++)
+		{
+			System.out.println("enter the amount for "+(i+1) +" Customer");
+			q1.enqueue(Util.readint());
+			System.out.println("Enter the type of transection like dep:-Deposit or wid:-for Withdral");
+			arr[i]=Util.readnext();
+			
+		}
+		money=q1.Balance(arr);
+		if(money>1)
+			System.out.println("Avilable amount in the bank is "+money);
+		
+	}
+}
