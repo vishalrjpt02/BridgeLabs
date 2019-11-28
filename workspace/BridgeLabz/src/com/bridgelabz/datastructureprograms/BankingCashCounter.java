909cbf6651d485911eb7baadcbfe3242db1d2fcf
vishalrjpt02 <vishalrjpt19@gmail.com>
2019-11-28 18:37:43 +0530

    [Vishal].add BankingCashCounter.java logic update

--- workspace/BridgeLabz/src/com/bridgelabz/datastructureprograms/BankingCashCounter.java
+++ workspace/BridgeLabz/src/com/bridgelabz/datastructureprograms/BankingCashCounter.java
@@ -6,21 +6,22 @@ public class BankingCashCounter {
 	public static void main(String[] args) 
 	{
 		System.out.println("enter number of people that can be in queue");
-		int num=Util.readint();
+		int CountofCustomers=Util.readint();
 		int money=0;
-		String arr[]=new String[num];
-		Queue q1=new Queue();
-		for(int i=0;i<num;i++)
+		String TypeofTranasection[]=new String[CountofCustomers];
+		Queue listingthecustomer=new Queue();
+		for(int i=0;i<CountofCustomers;i++)
 		{
 			System.out.println("enter the amount for "+(i+1) +" Customer");
-			q1.enqueue(Util.readint());
+			listingthecustomer.enqueue(Util.readint());
 			System.out.println("Enter the type of transection like dep:-Deposit or wid:-for Withdral");
-			arr[i]=Util.readnext();
+			TypeofTranasection[i]=Util.readnext();
 			
 		}
-		money=q1.Balance(arr);
+		money=listingthecustomer.Balance(TypeofTranasection);
 		if(money>1)
 			System.out.println("Avilable amount in the bank is "+money);
+		
 		
 	}
 }
