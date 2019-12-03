10edba482fbfbd5ebe493f68cd7b2f6cc83c5260
vishalrjpt02 <vishalrjpt19@gmail.com>
2019-12-03 12:53:25 +0530

    [vishal kumar] services.add stockInterface and commenting has been added

--- workspace/BridgeLabz/OOps/com/blbz/services/StockInterface.java
+++ workspace/BridgeLabz/OOps/com/blbz/services/StockInterface.java
@@ -1,11 +1,20 @@
+/************************************************************************************************
+ * This file contains all abtract methods,this the interface for project
+ * @author 	vishal kumar
+ * @version 1.0
+ * @date	02/12/19
+ * @purpose	to provide the interface for program
+ *************************************************************************************************/
+
 package com.blbz.services;
 
 public interface StockInterface {
 
 	void addshare();
 	void viewshare();
-	void nubmerofshare();
+	void numberofshare();
 	void sharePrice();
 	void stockreport();
+	void removeshare();
 	
 }
